bcda2cb412f40d4f8bdcaccd5e0fb560fe8a4a81
Tin Tun Hlaing <tintunhlaing223@gmail.com>
2026-05-27 08:59:37 +0630

    todo function

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-const hireButton = document.getElementById("hireBtn");
+const taskInput = document.getElementById("taskInput");
+const addBtn = document.getElementById("addBtn");
+const todoList = document.getElementById("todoList");
 
-hireButton.addEventListener('click', function () {
-    alert('Thank you for hiring Traco! Skill monster is ready to work.')
-});
+addBtn.onclick = function () {
+    let taskText = taskInput.value.trim();
+
+    if (taskText == "") {
+        alert("Pleace write something to work.");
+        return;
+    }
+
+    let li = document.createElement("li");
+    li.className = "flex justify-between items-center bg-gray-50 p-3 rounded-xl border border-gray-200 shadow-sm animate-fade-in";
+
+    li.innerHTML = `
+    <span class= "text-gray-800 font-medium">${taskText}</span>
+    <button class="deleteBtn text-red-500 hover:text-red-700 font-bold bg-red-50 hover:bg-red-100 px-3 py-1 rounded-lg transition duration-200"> Delete </button>
+    `;
+
+    li.querySelector('.deleteBtn').onclick = function () {
+        li.remove();
+    }
+
+    todoList.appendChild(li);
+
+    taskInput.value = "";
+}
 
 const myForm = document.getElementById("myForm");
 
